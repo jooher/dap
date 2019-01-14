a3f055c27a6937e63c0f25dcea688386e7ba1ed9
jooher <jooher@gmail.com>
2019-01-14 16:03:14 +0300

    dapify

--- docs/dapify.js
+++ docs/dapify.js
@@ -1,52 +1,61 @@
-const dapify=
-
-'dapify'.d("$code=.code $run=.code"
-	,'PRE contenteditable'
-		.d(".editor=#; ! .code; a!")
-		.a("#.innerHTML=$code:dapify")
-		.e("blur","$code=.editor.textContent")
-	,'buttons'.d(""
-		,'welcome'.d("? ($code .code)eq; ! welcome")
-		,'BUTTON'.d("? ($code $run)ne; ! `Execute").ui("$run=$code")
-		,'BUTTON'.d("? ($code .code)ne; ! `Reset").ui("$run=$code=.code")
-		//,'BUTTON'.d("! submit").ui(".form:submit")
-	)
-	,'run'.d("!! .style; inline $run")
-	,'FORM action="https://dapmx.org/playground.php" target="_blank" method="post"'.d(".form=#"
-		,'INPUT type="hidden" name="code"'.d("#.value=$code")
-	)
-)
-.DICT({
-	welcome	:"You're welcome to modify this code!",
-	submit	:"Open in separate window"
-})
-.FUNC({
-	convert	:{
-		dapify	:code	=>code
-				.replace(/</g,"&lt;").replace(/>/g,"&gt;")	// html
-				.replace(/( \/\/.+?)$/gm,"<i>$1</i>")		// comments
-				.replace(/('.+?')/g,"<em>$1</em>") 		// element signatures
-				.replace(/(\$[^\s=.;@:"()`]*)/g,"<b>$1</b>"),	// $status variables
-				
-		submit	:form	=>form.submit()
-	},
+(nodes=>{
 	
-	flatten	:{
-	},
+const
 
-	operate	: {
-		inline	:(value,alias,node)=>{
-			try{
-				eval(value)
-				.RENDER(null,node);
-			}
-			catch(e){
-				alert("error: ");
+hilite	= code=>code
+	.replace(/</g,"&lt;").replace(/>/g,"&gt;")	// html
+	.replace(/( \/\/.+?)$/gm,"<i>$1</i>")		// comments
+	.replace(/('.+?')/g,"<em>$1</em>") 		// element signatures
+	.replace(/(\$[^\s=.;@:"()`]*)/g,"<b>$1</b>"),	// $status variables
+	
+dapify	= dap &&
+
+	'dapify'.d("$edit=.code $run=.code $dirty= $own="
+		,'PRE contenteditable'
+			.d("! .code; a!")
+			.a("#.innerHTML=$run:hilite")
+			.e("keyup","$edit=#.textContent $dirty=($edit $run)ne $own=($edit .code)ne")
+		,'buttons'.d(""
+			,'welcome'.d("? $own:!; ! welcome")
+			,'BUTTON.reset'	.d("? $own; !! reset@title").ui("$run= $run=$edit=.code")
+			,'BUTTON.render'.d("? $dirty; !! render@title").ui("$run=$edit")
+			//,'BUTTON'.d("! submit").ui(".form:submit")
+		)
+		,'run'.d("!! .style; inline $run")
+		,'FORM action="https://dapmx.org/playground.php" target="_blank" method="post"'.d(".form=#"
+			,'INPUT type="hidden" name="code"'.d("#.value=$edit")
+		)
+	)
+	.DICT({
+		welcome	:"You're welcome to modify this code!",
+		render	:"Run this code",
+		reset	:"Reset to original code",
+		submit	:"Open in separate window"
+	})
+	.FUNC({
+		convert	:{
+			hilite,
+			submit	:form	=>form.submit()
+		},
+
+		operate	: {
+			inline	:(value,alias,node)=>{
+				try{
+					eval(value)
+					.RENDER(null,node);
+				}
+				catch(e){
+					alert("error: "+e.message);
+				}
 			}
 		}
-	}
-});
+	});
+	
+	nodes.forEach(dapify
+		? pre=>dapify.RENDER({code:pre.textContent,style:pre.getAttribute("data-style")},null,pre)
+		: pre=>pre.innerHTML=hilite(pre.innerHTML)
+	);
 
-[...document.getElementsByTagName("pre")]
-	.filter	(pre=>pre.getAttribute("lang")=="dap")
-	.forEach(pre=>dapify.RENDER({code:pre.textContent,style:pre.getAttribute("data-style")},null,pre));
+})(
+	[...document.getElementsByTagName("pre")].filter(pre=>pre.getAttribute("lang")=="dap")
+);
