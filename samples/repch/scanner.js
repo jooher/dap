4f265cbe789e40306f8e7813098d6efb9e532506
Maxim Sazhin <jooher@gmail.com>
2021-01-22 23:15:09 +0300

    scan

--- samples/repch/scanner.js
+++ samples/repch/scanner.js
@@ -22,54 +22,65 @@ const
 	
 let
 	track = null,
-	stop	= e => {track.stop(); scanner.parentNode&&scanner.parentNode.removeChild(scanner)};
+	stop	= e => {
+		track&&track.stop();
+		scanner.parentNode&&scanner.parentNode.removeChild(scanner);
+	};
 
 cancelbtn.onclick = stop;
 
 
 export default async where=>{
 	
+	let decoded=null;
+	
 	(where||document.body).appendChild(scanner);
 				
 	await
 		navigator.mediaDevices.getUserMedia({video:{facingMode:"environment",width:640}})
-		.then(stream=>video.srcObject=stream);//.catch(reject);
+		.then(stream=>{
+			if(stream){
+				video.srcObject=stream;
+				track	= video.srcObject.getVideoTracks()[0];
+			}
+			else alert("No stream");
+		})
+		.catch(alert);
+
+	if(track){
 		
-	track	= video.srcObject.getVideoTracks()[0],
-	video.play();
-	
-	let
-		context=null,
-		decoded=null,
-		w=0,h=0;
+		video.play();
 		
-	while (!decoded && track.readyState==="live") {// && !paused
-		if(video.videoWidth!=w){
-			canvas.width	= w = video.videoWidth;
-			canvas.height	= h = video.videoHeight;
-			console.log("video size: "+w+" x "+h);
-			context = canvas.getContext('2d');
-		}
-		if(w>0){
-			context.drawImage(video,0,0,w,h);
+		let
+			context=null,
+			w=0,h=0;
 			
-/*
-detector.detect(context.getImageData(0,0,w,h))////video
-				.then( barcodes => {
-					//barcodes.forEach(barcode => console.log(barcodes.rawValue))
-					decoded=barcodes[0].rawValue;
-				})
-				.catch(e=>{
-					console.error("Barcode Detection failed: " + e);
-					decoded="https://error.com";
-				});
-			
-*/
-		}
-		await timeout(25); //paused?1e3:
-	};
+		while (!decoded && track.readyState==="live") {// && !paused
+			if(video.videoWidth!=w){
+				canvas.width	= w = video.videoWidth;
+				canvas.height	= h = video.videoHeight;
+				console.log("video size: "+w+" x "+h);
+				context = canvas.getContext('2d');
+			}
+			if(w>0){
+				context.drawImage(video,0,0,w,h);
+				
+				detector.detect(context.getImageData(0,0,w,h))////video
+					.then( barcodes => {
+						//barcodes.forEach(barcode => console.log(barcodes.rawValue))
+						decoded=barcodes[0].rawValue;
+					})
+					.catch(e=>{
+						console.error("Barcode Detection failed: " + e);
+						decoded="https://error.com";
+					});
+			}
+			await timeout(25); //paused?1e3:
+		};
+	}
 	
 	stop();
 	console.log(decoded);
 	return decoded;//resolve(decoded); //"1234567890122";//
+	
 }
