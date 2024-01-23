41b0ad853f14ab52e2ba968be0755a31801a08fd
Maxim Sazhin <jooher@gmail.com>
2024-01-23 11:43:56 +0300

    Update poputka.js

--- samples/poputka/poputka.js
+++ samples/poputka/poputka.js
@@ -2,61 +2,66 @@ import "/0.5.js";
 import { api } from '/stuff/snippets.js';
 
 /*
-route, title:string, stops:text
 person, stars, info:json
-drive, route, person, seats, date:date, info:json
-hike, drive, person, stars, info:json
+route, title:string, stops:text
+ride, route, person, seats, date:date, info:json
+hike, ride, person, stars, info:json
 
 //vehicle, name, seats, info:json
-//driver, person, vehicle, plate:text, stars
+//rider, person, vehicle, plate:text, stars
 */
 
 
-"APP".d('$date= $from= $to= $drive= $person="1'
+"APP".d('$date= $from= $to= $ride= $person="1'
 
 	,"INPUT type=date".ui('$date=#:value')
 	
-	,"SELECT.from".d('',Routes).ui('$from=#:value')
-	,"SELECT.to".d('? $from',Routes).ui('$to=#:value')
+	,"SELECT.route".d('* ("route $from@stop):api'
+		,"OPTION".d('!! .name@ .route@value')
+	).ui('$route=#.value')
 	
-	,"driver".d('?? $tab@driver'
+	,"SELECT.from".d('',Places).ui('$from=#.value')
+	,"SELECT.to".d('? $from',Places).ui('$to=#.value')	
+	
+	,"driver".d('?? $tab@rider'
 		,"UL.hikes".d('* ("hike $route $date):api'
 			,"LI".d(Hike)
 		)
-		,"BUTTON.add-drive".d('? $person')
-			.ui('$drives=(@PUT"drive $person $route ($from $to)@info $date):api')
+		,"BUTTON.add-ride".d('? $person')
+			.ui('? $person $person=Auth():wait; $rides=(@PUT"ride $date $person $route ($from $to)@info):api')
 	)
 	
 	,"passenger".d('?? $tab@passenger'
-		,"UL.drives".d('* ("drive $route $date):api'
-			,"LI".d(Drive).ui("$drive=.")
+		,"UL.{name}s".d('* ("ride $route $from $to $date):api'
+			,"LI".d(Ride).ui("$ride=.")
 		)
-		,"BUTTON.seek-drives".ui('')
+		,"BUTTON.seek-rides".ui('')//subscribe for rides
 	)
 	
 	
 )
 
 .DICT({
-	Routes:[
-		// TODO: RecentRoutes
-		,"OPTGROUP".d('* (`route $from@stop):api'
-			,"OPTION".d('!! .title@ .route@value .stops@title')
+	Places:[
+		,"OPTGROUP".d('* recent.place@'
+			,"OPTION".d('!! .value .value@')
+		)
+		,"OPTGOUP".d('* ("place $from@stop):api'
+			,"OPTION".d('!! .name@ .name@value')
 		)
 	],
 	
-	Driver:
-	"driver".d(''
+	Person:
+	"person".d(''
 		,"name".d('! .name')
-		,"vehicle".d('! .vehicle')
 		,"stars".d('! .stars')
 	),
 	
-	Drive:
-	"drive".d('$?='
-		,"title".d('! .date .info.time .info.driver.name').ui('$?=$?:!')
-		,"details".d('? $?; * ("drive .drive):api; ! Driver(.driver@)',Passengers)
-		,"BUTTON.contact-driver".ui()
+	Ride:
+	"ride".d('$?='
+		,"title".d('! .date .info.time .info.rider.name').ui('$?=$?:!')
+		,"details".d('? $?; * ("ride .ride):api; ! rider(.rider@)',Passengers)
+		,"BUTTON.contact-rider".ui()
 	),
 	
 	Hike:
@@ -68,8 +73,8 @@ hike, drive, person, stars, info:json
 	Info:
 	"info".d(""
 		,"time".d("! .time")
-		,"driver".d("! .driver.name")
-		,"stars".d("! .driver.stars")
+		,"rider".d("! .rider.name")
+		,"stars".d("! .rider.stars")
 	)
 	
 })
